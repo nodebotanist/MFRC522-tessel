f5130379ed88eb5d35f64b9925096e190bf603be
Kas Perch <the@nodebotani.st>
2017-11-23 12:25:05 -0600

    Scaffolding MFRC522 object

--- MFRC522.js
+++ MFRC522.js
@@ -4,8 +4,21 @@ const EventEmitter = require('events').EventEmitter
 const util = require('util')
 const Tessel = require('tessel')
 
-function MFRC522() {
+function MFRC522(opts) {
+  if(!(this instanceof MFRC522)){
+    return new MFRC522(opts)
+  }
 
+  this.port = opts.port || 'A'
+  this.baud = opts.baud || 10000
+}
+
+MFRC522.prototype.writeToRegister = function(register) {
+
+}
+
+MFRC22.prototype.readFromRegister = function(register, numOfBytes, cb){
+  
 }
 
 util.inherits(MFRC522, EventEmitter)
