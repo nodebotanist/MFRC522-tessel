b2754c21125817fa6fcb767f3477760afd1ef350
Kas Perch <the@nodebotani.st>
2017-11-23 13:18:39 -0600

    Fixed reset bind

--- MFRC522.js
+++ MFRC522.js
@@ -15,46 +15,46 @@ const REGISTERS = {
   MODE: 0x11,
   TX_MODE: 0x12,
   RX_MODE: 0x13,
-  TX_CONTROL: 0x14,
+  TX_CONTROL: 0x14
 }
 
 class MFRC522 {
-  constructor(opts) {
+  constructor (opts) {
     if (!(this instanceof MFRC522)) {
-      return new MFRC522(opts);
+      return new MFRC522(opts)
     }
-    opts = opts || {};
-    this.port = Tessel.port[opts.port || 'A'];
-    this.baud = opts.baud || 4000000;
-    this.chipSelectPin = this.port.pin[opts.pin || 5];
-    this.resetPin = this.port.pin[opts.pin || 6];
+    opts = opts || {}
+    this.port = Tessel.port[opts.port || 'A']
+    this.baud = opts.baud || 4000000
+    this.chipSelectPin = this.port.pin[opts.pin || 5]
+    this.resetPin = this.port.pin[opts.pin || 6]
     this.spi = new this.port.SPI({
       clockSpeed: this.baud
     })
   }
-  init() {
+  init () {
     async.series([
       (cb) => {
-        this.chipSelectPin.write(1, cb);
+        this.chipSelectPin.write(1, cb)
       },
-      this.reset
+      this.reset.bind(this)
     ], () => {
       this.emit('ready')
     })
   }
-  reset(cb) {
+  reset (cb) {
     this.resetPin.write(0, () => {
       this.resetPin.write(1, cb)
     })
   }
-  writeToRegister(register, data, cb) {
+  writeToRegister (register, data, cb) {
     register = register << 1
-    register |= (0x01); // sets the read/write bit to write (1)
+    register |= (0x01) // sets the read/write bit to write (1)
     this.spi.transfer(Buffer.from([register].concat(data)), cb)
   }
-  readFromRegister(register, numOfBytes, cb) {
-    register = register << 1;
-    register &= ~(0x01); // sets the read/write bit to read (0)
+  readFromRegister (register, numOfBytes, cb) {
+    register = register << 1
+    register &= ~(0x01) // sets the read/write bit to read (0)
     this.spi.transfer(Buffer.from([register]), cb)
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict'
 // Import the interface to Tessel hardware
-const tessel = require('tessel')
 const MFRC522 = require('./MFRC522')
 
 let mfrc522 = new MFRC522()
