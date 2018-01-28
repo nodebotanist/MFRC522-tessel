e77a5d2cb7220fac4a0285166a5fa29c5f951329
Mx. Kas Perch <the@nodebotani.st>
2018-01-27 18:10:40 -0600

    added write function

--- MFRC522.js
+++ MFRC522.js
@@ -1,3 +1,6 @@
+const async = require('async')
+const Tessel = require('tessel')
+
 const PINS = {
   CHIP_SELECT: {
     port: 'A',
@@ -14,11 +17,18 @@ const PINS = {
 }
 
 const REGISTERS = {
-
 }
 
 class MFRC522 {
-
+  init () {
+    this.spi = new Tessel.port['A'].SPI({
+      clockSpeed: 4 * 1000 * 1000, // 4KHz
+      chipSelect: PINS.CHIP_SELECT
+    })
+  }
+  write (address, data, callback) {
+    this.spi.transfer(Buffer.from([(address << 1) & 0x7E].concat(data)), callback)
+  }
 }
 
 module.exports = MFRC522
