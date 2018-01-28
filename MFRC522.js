8f1723958aa23e87c8c441ae65ec52ec01fe6fbf
Mx. Kas Perch <the@nodebotani.st>
2018-01-27 18:51:18 -0600

    Refactored from class, added antennae functions

--- MFRC522.js
+++ MFRC522.js
@@ -17,43 +17,54 @@ const PINS = {
 }
 
 const REGISTERS = {
+  TX_CONTROL: 0x14
 }
 
-class MFRC522 {
-  init () {
-    this.spi = new Tessel.port['A'].SPI({
-      clockSpeed: 4 * 1000 * 1000, // 4KHz
-      chipSelect: PINS.CHIP_SELECT
-    })
-  }
+function MFRC522 () {
+  this.spi = null;
+}
 
-  write (register, data, callback) {
-    this.spi.transfer(Buffer.from([(register << 1) & 0x7E].concat(data)), callback)
-  }
+MFRC522.prototype.init = function () {
+  this.spi = new Tessel.port['A'].SPI({
+    clockSpeed: 4 * 1000 * 1000, // 4KHz
+    chipSelect: Tessel.port[PINS.CHIP_SELECT.port][PINS.CHIP_SELECT.pin]
+  })
+}
 
-  read (register, callback) {
-    this.spi.transfer(Buffer.from([((register << 1) & 0x7E) | 0x80, 0]), callback)
-  }
+MFRC522.prototype.write = function (register, data, callback) {
+  this.spi.transfer(Buffer.from([(register << 1) & 0x7E].concat(data)), callback)
+}
 
-  setBitMask (register, mask, callback) {
-    this.read(register, (err, data) => {
-      if (err) {
-        callback(err)
-      } else {
-        this.write(register, data | mask, callback)
-      }
-    })
-  }
+MFRC522.prototype.read = function (register, callback) {
+  this.spi.transfer(Buffer.from([((register << 1) & 0x7E) | 0x80, 0]), callback)
+}
 
-  clearBitMask (register, mask, callback) {
-    this.read(register, (err, data) => {
-      if (err) {
-        callback(err)
-      } else {
-        this.write(register, data & (~mask), callback)
-      }
-    })
-  }
+MFRC522.prototype.setBitMask = function (register, mask, callback) {
+  this.read(register, (err, data) => {
+    if (err) {
+      callback(err)
+    } else {
+      this.write(register, data | mask, callback)
+    }
+  })
+}
+
+MFRC522.prototype.clearBitMask = function (register, mask, callback) {
+  this.read(register, (err, data) => {
+    if (err) {
+      callback(err)
+    } else {
+      this.write(register, data & (~mask), callback)
+    }
+  })
+}
+
+MFRC522.prototype.antennaOn = function () {
+  this.setBitMask(REGISTERS.TX_CONTROL, 0x03)
+}
+
+MFRC522.prototype.antennaOff = function () {
+  this.clearBitMask(REGISTERS.TX_CONTROL, 0x03)
 }
 
 module.exports = MFRC522
