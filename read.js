5923d007c07c4bb7125edc86ec947c9a4f2613ac
Mx. Kas Perch <the@nodebotani.st>
2018-01-27 18:51:34 -0600

    Added antennaOn to read demo

--- read.js
+++ read.js
@@ -1,5 +1,6 @@
 const Tessel = require('tessel-io')
 const Five = require('johnny-five')
+const async = require('async')
 const MFRC522 = require('./MFRC522')
 
 let board = new Five.Board({
@@ -9,4 +10,14 @@ let board = new Five.Board({
 board.on('ready', () => {
   console.log('ready!')
   let reader = new MFRC522()
+
+  async.series([
+    reader.init,
+    reader.antennaOn
+  ], (err) => {
+    if (err) {
+      throw err
+    }
+    console.log('Reader ready!')
+  })
 })
